37b2c413f3b7d8c4904097c7170beb3c55198c1e
redis <redshehu1@gmail.com>
2025-03-02 21:22:15 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+// Test if our JS file is properly linked to the HTML File
+console.log(123);
 const hamburgerMenu = document.querySelector(".header__main-nav--hamburger");
+const headerMainNav = document.querySelector(".header__main-nav");
 const navMenu = document.querySelector(".header__main-nav--menu > div");
 
 hamburgerMenu.addEventListener("click", function () {
   navMenu.classList.toggle("nav-menu-clicked");
+  headerMainNav.classList.toggle("zindex");
 });
