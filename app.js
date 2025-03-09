586b2d12c48cd8acc8e3b7b90b8b7690c6889119
redis <redshehu1@gmail.com>
2025-03-09 16:29:57 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Test if our JS file is properly linked to the HTML File
 console.log(123);
-const hamburgerMenu = document.querySelector(".header__main-nav--hamburger");
+const hamburgerMenu = document.querySelector(".header__hamburger");
 const headerMainNav = document.querySelector(".header__main-nav");
 const navMenu = document.querySelector(".header__main-nav--menu");
 
